370df379800854f824c3618e048c60cef0a88a45
fariq <tonykriss2323@gmail.com>
2024-09-25 10:30:45 +0700

    m

---
+++ questions.js
@@ -0,0 +1,14 @@
+    const questions = [
+        {
+            time: 741.3,
+            question: '',
+            options: ["a", "b", "c", "d"],
+            correctAnswer: 0
+        },
+        {
+            time: 742.3,
+            question: "",
+            options: ["a", "b", "c", "d"],
+            correctAnswer: 2
+        }
+    ];
